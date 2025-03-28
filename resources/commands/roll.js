80b257dffeda6824e94b3f56d9929f0c9442c2a8
Nick <nicholas@petrunti.com>
2025-03-28 14:45:34 -0400

    Updated

---
+++ resources/commands/roll.js
@@ -0,0 +1,18 @@
+import { SlashCommandBuilder } from "discord.js";
+const inDev = false
+
+const data = new SlashCommandBuilder()
+    .setName("roll")
+    .setDescription("Generates a random number")
+    .addIntegerOption(option =>
+        option
+            .setName("maximum")
+            .setDescription("Highest value to be rolled")
+            .setRequired(true))
+
+async function execute(interaction) {
+    const max = interaction.options.getInteger("maximum");
+    await interaction.reply(`\`${Math.random()*(max - 0) + 0}`)
+}
+
+export {data, execute, inDev}
