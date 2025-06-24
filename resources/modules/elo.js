e1c86e13d4d2f43305f7fcc40f37b183e37dde97
Nick <nicholas@petrunti.com>
2025-06-24 02:25:59 -0400

    Updated

--- resources/modules/elo.js
+++ resources/modules/elo.js
@@ -4,14 +4,10 @@ import * as discord from "discord.js"
 
 async function init(user) {
     const eloDB = await database.fetchDatabase("scores")
-    console.log("initing")
     await eloDB.insertOne({
         user: user.id,
         elo: 600
     })
-
-    console.log("inserted")
-
 }
 
 export async function update(user, amount) {
@@ -21,25 +17,19 @@ export async function update(user, amount) {
     currentDocument = await eloDB.findOne({user:user.id})
 
     const currentElo = currentDocument.elo
-    await eloDB.updateOne({user: user.id}, {amount: currentElo+amount})
+    await eloDB.updateOne({user: user.id}, {$set: {amount: currentElo+amount}})
 
     return currentElo+amount
-
 }
 
 export async function check(user) {
     const eloDB = await database.fetchDatabase("scores")
     const currentDocument = await eloDB.findOne({user:user.id})
-    console.log(currentDocument)
     if( currentDocument === null ) {
        await init(user)
     }
 
-    console.log("running it up")
-
     const elo = await eloDB.findOne({user:user.id})
-    console.log(elo)
 
     return elo.elo
-
 }
