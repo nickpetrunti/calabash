1e0763ea46cd05bd9d3e0375fb62c079854c9b90
Nick <nicholas@petrunti.com>
2025-06-23 23:39:48 -0400

    Updated

--- resources/commands/delwarn.js
+++ resources/commands/delwarn.js
@@ -20,7 +20,7 @@ async function execute(interaction) {
     const warningDB = await database.fetchDatabase("warnings");
     const warnID = interaction.options.getString("warn-id");
 
-    console.log(await warningDB.findOne({"_id": new ObjectId(warnID)}));
+    console.log(await warningDB.findOne({"_id": new ObjectId(parseInt(warnID))}));
     const res1 = await warningDB.deleteOne({_id: warnID});
 
     const res = await warningDB.deleteOne({id: parseInt(warnID)});
