8889e2cf772dbea08050f6c5907761cc65437357
Nick <nicholas@petrunti.com>
2025-07-01 23:20:22 -0400

    Updated

--- resources/commands/warns.js
+++ resources/commands/warns.js
@@ -43,8 +43,8 @@ async function execute(interaction) {
         .setColor(0xA7DB7D)
         .setTitle(`Warnings: ${target.tag} (${target.id})`)
 
-    if (warnings.map.length < 1) {
-        console.log(warnings.length)
+    if (warnings.length < 1) {
+
         await interaction.reply({content: `No warnings to display for <@${target.id}>`, flags:[MessageFlags.Ephemeral]})
         return
     }
