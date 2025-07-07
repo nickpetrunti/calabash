210135556fffcd0f5d8ac66c7f1f36dea9f2e2b8
Nick <nicholas@petrunti.com>
2025-07-07 14:17:08 -0400

    Updated

---
+++ resources/commands/container.js
@@ -0,0 +1,64 @@
+import fetch from "node-fetch";
+import JSONbig from "json-bigint"
+import {
+    ContainerBuilder,
+    SlashCommandBuilder,
+    TextDisplayBuilder,
+    SectionBuilder,
+    SeparatorSpacingSize,
+    MessageFlags,
+    ThumbnailBuilder
+} from "discord.js";
+import {container} from "../embeds/calabash-release.js";
+
+const inDev = true;
+const commandType = "moderation"
+const data = new SlashCommandBuilder()
+    .setName("container")
+    .setDescription("Dynamically generates an embed container based on provided JSON data")
+    .addStringOption(option =>
+        option
+            .setName("json")
+            .setDescription("URL of the JSON data file")
+            .setRequired(true))
+
+async function execute(interaction) {
+    const dataURL = interaction.options.getString("url");
+    const dataFetch = await fetch(dataURL);
+    const dataBuffer = await dataFetch.buffer();
+    const dataString = dataBuffer.toString();
+    const data = JSONbig.parse(dataString)
+
+    try {
+        const container = new ContainerBuilder()
+        data.forEach(sectionData => {
+            if (sectionData.type === "text") {
+                if(!sectionData.image) {
+                    container.addTextDisplayComponents(new TextDisplayBuilder().setContent(sectionData.content.join("\n")))
+                } else {
+                    const section = new SectionBuilder()
+                        .addTextDisplayComponents(new TextDisplayBuilder().setContent(sectionData.content.join("\n")))
+                        .setThumbnailAccessory(new ThumbnailBuilder({
+                            description: "image",
+                            media: {
+                                url: sectionData.image
+                            }
+                        }))
+                    container.addSectionComponents(section)
+                }
+            } else if(sectionData.type === "separator-large") {
+                container.addSeparatorComponents(separator => separator.setSpacing(SeparatorSpacingSize.Large));
+            } else if(sectionData.type === "separator-small") {
+                container.addSeparatorComponents(separator => separator.setSpacing(SeparatorSpacingSize.Small));
+            }
+        })
+        await interaction.channel.send({components: [container], flags: MessageFlags.IsComponentsV2})
+    } catch(e) {
+        console.warn(e)
+        await interaction.reply({content: "There was an error while parsing the JSON data."})
+    }
+
+}
+
+
+export {data, execute, inDev, commandType}
